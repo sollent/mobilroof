109d7a76f85d1e0d1c0cdea1c2f057afb64c42b2
Pavel Laikov <pavel.laikov98@gmail.com>
2019-10-27 23:31:27 +0300

    Add bot

--- src/Controller/ViberController.php
+++ src/Controller/ViberController.php
@@ -3,9 +3,12 @@
 namespace App\Controller;
 
 use App\Message\ViberNotification;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Viber\Api\Sender;
+use Viber\Client;
 
 /**
  * Class ViberController
@@ -22,6 +25,25 @@ class ViberController extends AbstractController
      */
     public function eventCreateAction(Request $request)
     {
-        $this->dispatchMessage(new ViberNotification('Привеееет!!!'));
+//        $this->dispatchMessage(new ViberNotification('Привеееет!!!'));
+        $apiKey = '4a7808342d67d79f-ce040566e89a60fd-cafc75380821a4c3'; // from "Edit Details" page
+        $webhookUrl = 'https://api.einstein.by/send-message'; // for exmaple https://my.com/bot.php
+
+        try {
+            $client = new Client([ 'token' => $apiKey ]);
+            $result = $client->setWebhook($webhookUrl);
+            echo "Success!\n";
+        } catch (Exception $e) {
+            echo "Error: ". $e->getError() ."\n";
+        }
+
+        $botSender = new Sender([
+            'name' => 'Reply bot',
+            'avatar' => 'https://developers.viber.com/img/favicon.ico',
+        ]);
+
+        $client->sendMessage((new \Viber\Api\Message\Text())
+            ->setSender($botSender)
+            ->setText('ddadsdas'));
     }
 }
