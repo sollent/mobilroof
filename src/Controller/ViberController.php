e15097449c8ad9f8582f8dfde6caecd146f37cc6
sollent <pavel.laikov98@gmail.com>
2019-11-01 19:39:10 +0300

    Add bot

--- src/Controller/ViberController.php
+++ src/Controller/ViberController.php
@@ -20,14 +20,11 @@ class ViberController extends AbstractController
 {
     /**
      * @Route(
-     *     "/send-message"
+     *     "/subscribe"
      * )
-     *
-     * @param Request $request
      */
-    public function eventCreateAction(Request $request)
+    public function subscribe()
     {
-//        $this->dispatchMessage(new ViberNotification('Привеееет!!!'));
         $apiKey = '4a89ee2bfe27d633-5b00caedd2d50e09-71aa79f3c8acdc93'; // from "Edit Details" page
         $webhookUrl = 'https://api.einstein.by/send-message'; // for exmaple https://my.com/bot.php
 
@@ -37,7 +34,28 @@ class ViberController extends AbstractController
             echo "Success!\n";
         } catch (\Exception $e) {
             echo "Error: ". $e->getMessage() ."\n";
+        } // from "Edit Details" page
+        $webhookUrl = 'https://api.einstein.by/send-message'; // for exmaple https://my.com/bot.php
+
+        try {
+            $client = new Client([ 'token' => $apiKey ]);
+            $result = $client->setWebhook($webhookUrl);
+            echo "Success!\n";
+        } catch (\Exception $e) {
+            echo "Error: ". $e->getMessage() ."\n";
         }
+    }
+
+    /**
+     * @Route(
+     *     "/send-message"
+     * )
+     *
+     * @param Request $request
+     */
+    public function eventCreateAction(Request $request)
+    {
+        $apiKey = '4a89ee2bfe27d633-5b00caedd2d50e09-71aa79f3c8acdc93';
 
         $botSender = new Sender([
             'name' => 'Reply bot',
